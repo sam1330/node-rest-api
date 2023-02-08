aff633527f88b44c823c85276b8250d5c9eddbda
Samuel Martinez <sammy1301m@gmail.com>
2023-02-08 14:06:38 -0400

    feat: storing users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,21 +1,25 @@
 import { getConnection } from "../database/database";
 
 const getUsers = async (req, res) => {
-  try {
-    const connection = await getConnection();
-    const result = await connection.query("SELECT id, name FROM users");
-    res.json(result);
-  } catch (error) {
-    res.status(500);
-    res.send(error.message);
-  }
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, name FROM users");
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
 };
 
 const storeUser = async (req, res) => {
     try {
-        console.log(req.body);
+        const { username, password, name } = req.body;
+        const newUser = { username, password, name };
         const connection = await getConnection();
-
+        const result = await connection.query(
+            "INSERT INTO users SET ?",
+            newUser
+        );
         res.send("User created");
     } catch (error) {
         res.status(500);
@@ -23,8 +27,7 @@ const storeUser = async (req, res) => {
     }
 };
 
-
 export const methods = {
-  getUsers,
-  storeUser
+    getUsers,
+    storeUser,
 };
